7a50723221ceb57f550e482ef375cc7ee0497030
LiuTao <liutao@nulltech.cn>
2025-08-14 13:54:55 +0800

    添加基本的待办事项功能，包括创建、列出和删除待办事项的 API

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use tracing::info;
+use axum::{
+    Json, Router, extract::{State}, http::StatusCode, response::IntoResponse,
+    response::Response, routing::get,
+    routing::delete,
+};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::Mutex;
+
+// model definitions
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct Todo {
+    id: u32,
+    text: String,
+    done: bool,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct CreateTodo {
+    text: String,
+}
+
+// error
+#[derive(Serialize, Debug)]
+struct AppError {
+    message: String,
+}
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+    fn from(error: E) -> Self {
+        AppError {
+            message: error.into().to_string(),
+        }
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let body = axum::Json(self);
+        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+    }
+}
+
+//
+async fn list_todos(State(db): State<Arc<Mutex<HashMap<u32, Todo>>>>) -> Json<Vec<Todo>> {
+    info!("GET /todos");
+    let db = db.lock().await;
+    Json(db.values().cloned().collect())
+}
+
+async fn create_todo(
+    State(db): State<Arc<Mutex<HashMap<u32, Todo>>>>,
+    Json(payload): Json<CreateTodo>,
+) -> Result<(StatusCode, Json<Todo>), AppError> {
+    info!("POST /todos");
+    let mut db = db.lock().await;
+    let id = db.len() as u32 + 1;
+    let todo = Todo {
+        id,
+        text: payload.text,
+        done: false,
+    };
+    db.insert(id, todo.clone());
+    Ok((StatusCode::CREATED, Json(todo)))
+}
+
+async fn delete_todo(
+    State(db): State<Arc<Mutex<HashMap<u32, Todo>>>>,
+    axum::extract::Path(id): axum::extract::Path<u32>,
+) -> Result<StatusCode, AppError> {
+    info!("DELETE /todos/{}", id);
+    let mut db = db.lock().await;
+    if db.remove(&id).is_some() {
+        Ok(StatusCode::NO_CONTENT)
+    } else {
+        Err(AppError {
+            message: "Todo not found".to_string(),
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    // 内存数据库
+    let db = Arc::new(Mutex::new(HashMap::<u32, _>::new()));
+
+
+    let app = Router::new()
+        .route("/todos", get(list_todos).post(create_todo))
+        .route("/todos/{id}", delete(delete_todo))
+        .with_state(db);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
